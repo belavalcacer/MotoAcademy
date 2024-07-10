80cb859f9bc7a4f3a2d4e68fbe5273308f77c9ce
Sandra Valcacer <simv.snf22@uea.edu.br>
2024-07-10 18:47:45 -0400

    teste

---
+++ javascript/aula_08-07.js
@@ -0,0 +1,103 @@
+// let exemplo = "42"; 
+// let exemplo2 = "42"
+
+// let resultado = exemplo + exemplo;
+// //console.log(resultado);
+
+// resultado = "Flamengo";
+//console.log(resultado);
+
+//console.log("10" - "1");
+
+const numero = 1 //number
+const string = "1" //string
+const boolean = true //boolean true or false
+const array = [1,2,3,4,5] //array
+const objeto = {nome: "João", idade: 21} //object
+const funcao = function(){} //function
+const nulo = null; 
+
+const obejto = {
+    nome: "Sandra",
+    idade: 20
+}
+
+//objeto.nome = 'Linton'
+
+//const array2 = ['teste', 1, true]
+
+//const soma = 23 + 23;
+//const subtracao = 2 - 1;
+//const multiplicacao = 2 * 2;
+//const divisao = 22 / 2;
+
+//variaveis
+// apenasVariavel = 'valor'; // só tem acesso dentro de fuction
+// const exemploConst = 'constante'; // definir um valor fixo e exclusivo
+// const array3 = [];
+// //array3.push(24); // modelo em que consigo fazer alteracao em uma const
+// //console.log(array3);
+
+// const objeto1 = {
+//     aluno: 'Evelyn',
+// }
+// objeto.aluno = 'Sandra';
+//console.log(objeto);
+
+//let variavel2 = 'valor fora'; // consigo mudar
+
+//function teste () {
+ //   let variavel2 = 'valor dentro';
+ //   variavel2 = 'mudamos o valor dentro'
+ //   console.log(variavel2);
+    //teste2(variavel2);
+//}
+
+
+//function teste2 (variavel) {
+ //   variavel2 = 'mudou 2';
+ //   variavel = 'mudou 3';
+//}
+
+// var button = 'oi'; 
+
+// function teste (){
+//     teste2();
+// }
+
+// function teste2 () {
+//     variavelGlobal = 'tchau';
+//     }
+
+// teste();
+// console.log(variavelGlobal);
+
+// const aluno = 'Sandra';
+// if (aluno == 'Evelyn') {
+//     console.log('Evelyn');
+// } else {
+//     console.log('Sandra');
+// }
+
+// let numero1 = 22;
+// if (numero1 === '22') {
+//     console.log('Ele e igual');
+// } else {
+//     console.log('Ele e diferente');
+// }
+
+// if(30>numero){
+// console.log("Maior")
+// } else if (30 != numero) {
+//     console.log("Eh diferente")
+// }
+
+const numeroo = 5;
+
+if (numeroo > 2 && numero < 6) {
+    console.log('Está entre 2 e 6');
+}
+
+if (numero > 2 || numero > 6) {
+    console.log('Está entre 2 e 6 dnv');
+}
